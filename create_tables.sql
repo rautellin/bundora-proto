e9578543c36a3adaf532525fd40662745bf90c58
rautellinn <nathalie@rautellinn.com>
2024-09-02 18:32:32 +0200

    chore: updated sql tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,15 +1,21 @@
+CREATE TABLE features (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    created_at TIMESTAMPTZ NOT NULL
+);
+
 CREATE TABLE profiles (
     id UUID PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ,
     deleted_at TIMESTAMPTZ,
-    partner_id UUID REFERENCES profiles(id),
+    partner_id UUID REFERENCES profiles(id) CHECK (partner_id IS NULL OR partner_id <> id),
     onboarding_completed BOOLEAN NOT NULL DEFAULT FALSE,
-    fcm_token TEXT REFERENCES tokens(token),
     name TEXT,
     character_type character_type NOT NULL,
     avatar_type avatar_type NOT NULL,
-    current_mood mood_type NOT NULL,
+    current_mood mood_type NOT NULL
 );
 
 CREATE TABLE tokens (
@@ -17,7 +23,7 @@ CREATE TABLE tokens (
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ,
     token TEXT NOT NULL,
-    profile_id UUID NOT NULL REFERENCES profiles(id),
+    profile_id UUID NOT NULL REFERENCES profiles(id)
 );
 
 CREATE TABLE events (
@@ -34,7 +40,7 @@ CREATE TABLE events (
     enhanced BOOLEAN NOT NULL DEFAULT FALSE,
     mood mood_type,
     reaction TEXT,
-    message TEXT,
+    message TEXT
 );
 
 CREATE TABLE posts (
@@ -49,27 +55,20 @@ CREATE TABLE posts (
     seen BOOLEAN NOT NULL DEFAULT FALSE,
     type post_type NOT NULL,
     mood mood_type NOT NULL,
-    tags tag_type[],
+    tags tag_type[]
 );
 
 CREATE TABLE passcodes (
     id UUID PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL,
     code TEXT NOT NULL,
-    created_by UUID NOT NULL REFERENCES profiles(id),
+    created_by UUID NOT NULL REFERENCES profiles(id)
 );
 
 CREATE TABLE profile_onboarding (
+    PRIMARY KEY (profile_id, feature_id),
     profile_id UUID REFERENCES profiles(id) ON DELETE CASCADE,
     feature_id UUID REFERENCES features(id) ON DELETE CASCADE,
     onboarded BOOLEAN NOT NULL DEFAULT FALSE,
-    onboarded_at TIMESTAMPTZ,
-    PRIMARY KEY (profile_id, feature_id)
-);
-
-CREATE TABLE features (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT,
-    created_at TIMESTAMPTZ NOT NULL,
+    onboarded_at TIMESTAMPTZ
 );
