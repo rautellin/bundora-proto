eec5be583291954c00e318bae840a37a628ae39c
rautellinn <nathalie@rautellinn.com>
2024-09-02 20:33:53 +0200

    chore: recover file for updating profiles

---
+++ profiles_update.sql
@@ -0,0 +1,15 @@
+UPDATE public.profiles SET
+    partner_id = '9cf5bd55-ba56-4748-965b-3201b7f35384'::uuid WHERE
+    id = 'f274d1fc-3e0f-433a-bc07-236cf4a01b55';
+
+UPDATE public.profiles SET
+    partner_id = 'f274d1fc-3e0f-433a-bc07-236cf4a01b55'::uuid WHERE
+    id = '9cf5bd55-ba56-4748-965b-3201b7f35384';
+
+UPDATE public.profiles SET
+    partner_id = '334d894c-a2cf-4793-a478-54fb60671dba'::uuid WHERE
+    id = '187c68a0-8668-4767-b15d-31e9d5a2f7fe';
+
+UPDATE public.profiles SET
+    partner_id = '187c68a0-8668-4767-b15d-31e9d5a2f7fe'::uuid WHERE
+    id = '334d894c-a2cf-4793-a478-54fb60671dba';
